757cd30a84143ac5afa385980e4d9673841ce847
Terzich <ahmedterzic@hotmail.com>
2021-08-03 16:22:55 +0200

    Homepage design

--- Taxi-Plus-App/src/app/app-routing.module.ts
+++ Taxi-Plus-App/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { AppComponent } from "./app.component";
 import { HomepageComponent } from "./homepage/homepage.component";
 
 const routes: Routes = [
