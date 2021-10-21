908c91e1676c0bb9d73e6619a692bcbfffb829e7
Terizch <ahmedterzic@hotmail.com>
2021-10-21 16:58:15 +0200

    Bootstrap Modal (Rent-Request component)

--- Taxi-Plus-App/src/app/app.module.ts
+++ Taxi-Plus-App/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { CarDetailsComponent } from './car-list/car-details/car-details.componen
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RentCarComponent } from './car-list/rent-car/rent-car.component';
 import { HttpClientModule } from '@angular/common/http';
+import { RentRequestComponent } from './car-list/rent-car/rent-request/rent-request.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { HttpClientModule } from '@angular/common/http';
     CompanyOfferComponent,
     CarListComponent,
     CarDetailsComponent,
-    RentCarComponent
+    RentCarComponent,
+    RentRequestComponent
   ],
   imports: [
     BrowserModule,
